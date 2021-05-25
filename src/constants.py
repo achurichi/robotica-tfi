97056db6c9890ca024abbfc457ae8db18159ad92
Alan Churichi <achurichi@gmail.com>
2021-05-24 21:02:18 -0300

    The robot now leaves the boxes on different places

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,9 @@
 ROBOT_NAME = 'IRB120'  # Nombre del robot el Coppelia
 JOINTS_NUM = 6         # Cantidad de articulaciones
 CHECKPOINTS_TRAJ_1_NUM = 2    # Cantidad de Checkpoints
-CHECKPOINTS_PIONEER_NUM = 3
+CHECKPOINTS_PIONEER_NUM = 6
 CHECKPOINTS_BOXES_NUM = 2
-CHECKPOINTS_NUM = 7    # Cantidad de Checkpoints
 BOX_COUNT = 3
+POINTS_FAST_TRANSLATION = 50  # Movimiento rápido
+POINTS_MID_TRANSLATION = 150  # Movimiento moderado
+POINTS_SLOW_TRANSLATION = 250  # Movimiento lento
